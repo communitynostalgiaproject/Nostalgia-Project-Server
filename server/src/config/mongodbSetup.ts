6a998517f414026b576a634291b313f8a36e684e
David White <whited9339@gmail.com>
2023-10-23 10:51:01 -0600

    Implemented experience creation endpoint

--- server/src/config/mongodbSetup.ts
+++ server/src/config/mongodbSetup.ts
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
+    await mongoose.connect(
+      process.env.MONGODB_URI as string,
+      {
+        dbName: "food-nostalgia-data"
+      }
+    );
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
     process.exit(1);
--- server/src/controllers/experiences.controller.ts
+++ server/src/controllers/experiences.controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { NotFoundError } from "../utils/customErrors";
+import { Experience } from "@shared/types/experience";
+import utils from "../utils/experienceUtils";
 
-exports.createExperience = (req: Request, res: Response, next: NextFunction) => {
-  // To do
-  res.send("Route not yet implemented.");
+exports.createExperience = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const experience: Experience = req.body;
+    const savedExperience = await utils.createExperience(experience);
+
+    res.status(200).send(savedExperience);
+  } catch(err) {
+    console.log(err);
+    next(err);
+  }
 };
 
 exports.getExperienceById = (req: Request, res: Response, next: NextFunction) => {
@@ -23,7 +32,7 @@ exports.updateExperience = (req: Request, res: Response, next: NextFunction) => 
 
 exports.deleteExperience = (req: Request, res: Response, next: NextFunction) => {
   // To do
-  res.send("Route not yet implemented.");
+  res.send("Route not yet implemented.");``
 };
 
 export default exports;
