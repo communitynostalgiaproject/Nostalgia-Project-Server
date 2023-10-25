c0ec1f6bc92bed4b86f8437474f03945368cbade
David White <whited9339@gmail.com>
2023-10-25 07:16:13 -0600

    Updated testDataGen util to make better mock experiences

--- server/src/utils/testDataGen.ts
+++ server/src/utils/testDataGen.ts
@@ -5,7 +5,7 @@ import { Experience } from "@shared/types/experience";
 import { ContentType, Flag } from "@shared/types/flag";
 import { ObjectId } from "mongoose";
 
-export const createUsers = (n: number) => {
+export const createUsers = (n: number, areModerators = false, areAdmins = false) => {
   if (n < 1) return [];
 
   const users = [];
@@ -13,10 +13,10 @@ export const createUsers = (n: number) => {
   for (let i = 0; i < n; i++) {
     users.push({
       googleId: faker.string.uuid(),
-      displayName: faker.name.fullName(),
+      displayName: faker.person.fullName(),
       emailAddress: faker.internet.email(),
-      isModerator: Boolean(randomInt(2)),
-      isAdmin: Boolean(randomInt(2)),
+      isModerator: areModerators,
+      isAdmin: areAdmins,
       joinedDate: faker.date.past()
     } as User);
   }
@@ -24,7 +24,211 @@ export const createUsers = (n: number) => {
   return users;
 };
 
-export const createExperiences = (n: number) => {
+const moods = [
+  "Happy",
+  "Sad",
+  "Angry",
+  "Anxious",
+  "Relieved",
+  "Confident",
+  "Hopeful",
+  "Frustrated",
+  "Bored",
+  "Excited",
+  "Calm",
+  "Content",
+  "Overwhelmed",
+  "Indifferent",
+  "Nervous",
+  "Grateful",
+  "Guilty",
+  "Fearful",
+  "Joyful",
+  "Lonely",
+  "Curious",
+  "Apathetic",
+  "Optimistic",
+  "Pessimistic",
+  "Envious",
+  "Jealous",
+  "Inspired",
+  "Motivated",
+  "Relaxed",
+  "Resentful"
+];
+
+const foodTypes = [
+  "Fruits",
+  "Vegetables",
+  "Grains",
+  "Protein",
+  "Dairy",
+  "Nuts and Seeds",
+  "Legumes",
+  "Sweets",
+  "Beverages",
+  "Baked Goods",
+  "Snacks",
+  "Fast Food",
+  "Seafood",
+  "Condiments",
+  "Herbs and Spices",
+  "Frozen Foods",
+  "Canned Goods",
+  "Prepared Meals",
+  "International Cuisine",
+  "Gluten-Free",
+  "Vegan",
+  "Vegetarian",
+  "Organic",
+  "Non-GMO",
+  "Low Carb",
+  "Low Fat",
+  "Low Sodium",
+  "High Protein",
+  "Keto",
+  "Paleo"
+];
+
+const peopleToBeRemindedOf = [
+  "Childhood Friend",
+  "High School Sweetheart",
+  "College Roommate",
+  "Favorite Teacher",
+  "First Boss",
+  "Mentor",
+  "Neighbor From Home Town",
+  "Summer Camp Buddy",
+  "Travel Companion",
+  "Family Friend",
+  "Old Rival",
+  "Team Captain",
+  "Band Mate",
+  "Art Class Partner",
+  "Lab Partner",
+  "Library Study Buddy",
+  "First Date",
+  "Dance Partner",
+  "Karaoke Partner",
+  "Cooking Class Pal",
+  "Gym Buddy",
+  "Yoga Instructor",
+  "Music Teacher",
+  "Sports Coach",
+  "Childhood Hero",
+  "Parent's Friend",
+  "Cousin's Best Friend",
+  "Sibling's Ex",
+  "Former Colleague",
+  "Chess Opponent",
+  "Mother",
+  "Father",
+  "Sibling",
+  "Brother",
+  "Sister",
+  "Grandmother",
+  "Grandfather",
+  "Aunt",
+  "Uncle",
+  "Cousin",
+  "Niece",
+  "Nephew",
+  "Best Friend"
+];
+
+const flavourProfiles = [
+  "Sweet",
+  "Sour",
+  "Salty",
+  "Bitter",
+  "Umami",
+  "Spicy",
+  "Savory",
+  "Tangy",
+  "Citrus",
+  "Earthy",
+  "Herbaceous",
+  "Fruity",
+  "Floral",
+  "Nutty",
+  "Smoky",
+  "Woody",
+  "Minty",
+  "Creamy",
+  "Rich",
+  "Crisp",
+  "Astringent",
+  "Metallic",
+  "Pungent",
+  "Sharp",
+  "Buttery",
+  "Mellow",
+  "Zesty",
+  "Peppery",
+  "Toasted",
+  "Vanilla",
+  "Chocolatey",
+  "Caramel"
+];
+
+const periodOfLifeAssociations = [
+  "Infancy",
+  "Toddlerhood",
+  "Early Childhood",
+  "Middle Childhood",
+  "Adolescence",
+  "Early Adulthood",
+  "Midlife",
+  "Mature Adulthood",
+  "Late Adulthood",
+  "Elderly",
+  "Quarter-life Crisis",
+  "Midlife Crisis",
+  "Empty Nest",
+  "Retirement",
+  "Golden Years",
+  "Second Childhood",
+  "Reinvention",
+  "Enlightenment",
+  "Self-Discovery",
+  "Adventure Phase",
+  "Sabbatical",
+  "Legacy Building",
+  "Wisdom Era",
+  "Exploration Stage",
+  "Reflection Period"
+];
+
+const createRandomPlaces = (n: number) => {
+  if (n < 1) return [];
+
+  const places  = [];
+
+  for (let i = 0; i < n; i++) {
+    places.push({
+      address: {
+        gid: faker.string.uuid(),
+        sourceId: faker.string.uuid(),
+        name: faker.location.streetAddress(),
+        accuracy: "point",
+        country: faker.location.country(),
+        street: faker.location.street(),
+        locality: faker.location.city()
+      },
+      location: {
+        type: "Point",
+        coordinates: [
+          faker.location.latitude(),
+          faker.location.longitude()
+        ]
+      }
+    });
+  }
+
+  return places;
+}
+
+export const createExperiences = (n: number, userIds: ObjectId[] = []) => {
   if (n < 1) return [];
 
   const experiences = [];
@@ -32,29 +236,20 @@ export const createExperiences = (n: number) => {
   for (let i = 0; i < n; i++) {
     experiences.push({
       title: faker.lorem.words(randomInt(3, 6)),
-      place: {
-        address: {
-          gid: faker.string.uuid(),
-          sourceId: faker.string.uuid(),
-          name: faker.location.streetAddress(),
-          accuracy: "point",
-          country: faker.location.country(),
-          street: faker.location.street(),
-          locality: faker.location.city()
-        },
-        location: {
-          type: "Point",
-          coordinates: [
-            faker.location.latitude(),
-            faker.location.longitude()
-          ]
-        }
-      },
+      place: createRandomPlaces(1)[0],
       description: faker.lorem.paragraphs({
         min: 1,
         max: 3
       }),
-      experienceDate: faker.date.past().toISOString()
+      createdDate: faker.date.past().toISOString(),
+      experienceDate: faker.date.past().toISOString(),
+      mood: moods[randomInt(moods.length)],
+      foodtype: foodTypes[randomInt(foodTypes.length)],
+      personItRemindsThemOf: peopleToBeRemindedOf[randomInt(peopleToBeRemindedOf.length)],
+      flavourProfile: flavourProfiles[randomInt(flavourProfiles.length)],
+      periodOfLifeAssociation: periodOfLifeAssociations[randomInt(periodOfLifeAssociations.length)],
+      placesToGetFood: createRandomPlaces(randomInt(3)),
+      creatorId: Math.random() < 0.4 ? undefined : userIds[randomInt(userIds.length)]
     } as Experience);
   }
 
@@ -78,7 +273,7 @@ export const createFlags = (
       userId,
       createdDate: faker.date.past(),
       priority: ["low", "medium", "high"][randomInt(3)],
-      reason: ["spam", "hate-speech", "misinformation", "other"][4],
+      reason: ["spam", "hate-speech", "misinformation", "other"][randomInt(4)],
       userComment: faker.lorem.sentences({
         min: 1,
         max: 6
