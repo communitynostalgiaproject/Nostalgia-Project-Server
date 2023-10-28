f2e58a68a963e180041d227775d1fdb0f0acafa2
David White <whited9339@gmail.com>
2023-10-28 12:52:18 -0600

    Set up integration testing and implemented first test for experience creation endpoint

---
+++ server/src/__tests__/integration/endpoints/experiences/createExperience.test.ts
@@ -0,0 +1,49 @@
+import request from "supertest";
+import { setupApp } from "../../../../config/app";
+import { createExperiences } from "../../../../utils/testDataGen";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { Express } from "express";
+
+let mongoServer: MongoMemoryServer;
+let app: Express;
+
+const removeMongooseDocFields: any = (obj: any) => {
+  if (typeof obj !== 'object' || obj === null) return obj;
+
+  if (Array.isArray(obj)) {
+    return obj.map((item: any) => removeMongooseDocFields(item));
+  }
+
+  const newObj: any = {};
+  for (let key in obj) {
+    if (key !== "__v" && key !== "_id") {
+      newObj[key] = removeMongooseDocFields(obj[key]);
+    }
+  }
+
+  return newObj;
+}
+
+describe("Experience creation endpoint tests", () => {
+  beforeAll(async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    app = setupApp(uri);
+  });
+  
+  afterAll(async () => {
+    await mongoServer.stop();
+  });
+
+  it("should return a 200 code and copy of created record on success", async () => {
+    const testExperience = createExperiences(1)[0];
+
+    const res = await request(app)
+      .post("/experiences")
+      .send(testExperience)
+      .set("Content-Type", "application/json");
+
+    expect(res.status).toBe(200);
+    expect(removeMongooseDocFields(res.body)).toEqual(testExperience);
+  });
+});
