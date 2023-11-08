69596f9ec91d1cc7e274f1e2dd4709ecf1394c2c
Nesmith-Charlesii <nesmith.charlesii@gmail.com>
2023-11-07 21:11:16 -0500

    limit query param test passed

--- server/src/__tests__/integration/endpoints/users/getUsers.test.ts
+++ server/src/__tests__/integration/endpoints/users/getUsers.test.ts
@@ -64,22 +64,29 @@ describe("GET /users", () => {
     }
     
     const res = await request(app).get(`/users`);
-    console.log("USER ARR", userArr)
+    
     expect(res.status).toBe(200);
     expect(res.body).toEqual(userArr);
   });
 
-//   it("should return a 500 code when attempting to update a record with an invalid id", async () => {
-//     const testUser = createUsers(1)[0];
-//     const insertedUser = await new UserModel(testUser).save();
-//     const updatedUser = {
-//       ...insertedUser.toObject(),
-//       _id: "111111111111",
-//       isAdmin: true
-//     };
+  it("should return a 200 code when successfully returning a limited amount of users", async () => {
+    const testUsers = createUsers(6);
+    const insertedUsers = await UserModel.insertMany(testUsers);
 
-//     const res = await request(app).patch(`/users`).send(updatedUser);
+    let userArr = [];
+    for (let userObj of insertedUsers) {
+      let user = convertValueToString(userObj.toObject());
+      userArr.push(user);
+    }
 
-//     expect(res.status).toBe(500);
-//   });
+    const res = await request(app).get(`/users`);
+    
+    const res_limit = await request(app)
+    .get(`/users`)
+    .query({limit: 2});
+    
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(userArr);
+    expect(res_limit.body.length).toEqual(2);
+  });
 });
