0d863ae522ef3145706fb88e3b291f5d234d500c
David White <whited9339@gmail.com>
2023-12-07 09:04:27 -0700

    Fixed isModerator and isAdmin assignment conditions

--- src/__tests__/integration/endpoints/experiences/deleteExperience.test.ts
+++ src/__tests__/integration/endpoints/experiences/deleteExperience.test.ts
@@ -8,6 +8,8 @@ import ExperienceModel from "../../../../models/experience.model";
 
 let mongoServer: MongoMemoryServer;
 let app: Express;
+let sessionCookie: string;
+let testUser: any;
 
 describe("DELETE /experiences/{experienceId}", () => {
   beforeAll(async () => {
@@ -15,10 +17,15 @@ describe("DELETE /experiences/{experienceId}", () => {
     const uri = mongoServer.getUri();
     await mongoose.connect(uri);
     app = setupApp(uri);
-  });
 
-  beforeEach(async () => {
-    await mongoose.connection.dropDatabase();
+    const authRes = await request(app).get("/auth/mock");
+    sessionCookie = authRes
+      .headers["set-cookie"][0]
+      .split(";")[0]
+      .trim();
+    testUser = authRes.body.user;
+    console.log(`testUser: ${JSON.stringify(testUser)}`);
+    console.log(`sessionCookie: ${sessionCookie}`);
   });
   
   afterAll(async () => {
@@ -28,11 +35,38 @@ describe("DELETE /experiences/{experienceId}", () => {
     }
   });
 
-  it("should return a 200 code upon success and should delete the db record", async () => {
+  it("should return a 401 code if user is not logged in", async () => {
     const testExperience = createExperiences(1)[0];
     const insertedExperience = await new ExperienceModel(testExperience).save();
 
     const res = await request(app).delete(`/experiences/${insertedExperience._id}`);
+
+    expect(res.status).toBe(401);
+  });
+
+  it("should return a 403 code if user does not have permission", async () => {
+    const testExperience = createExperiences(1)[0];
+    const insertedExperience = await new ExperienceModel(testExperience).save();
+
+    expect(insertedExperience._id).not.toBe(testUser._id);
+
+    const res = await request(app)
+      .delete(`/experiences/${insertedExperience._id}`)
+      .set("Cookie", sessionCookie);
+
+    expect(res.status).toBe(403);
+  });
+
+  it("should return a 200 code upon success and should delete the db record", async () => {
+    const testExperience = createExperiences(1)[0];
+    const insertedExperience = await new ExperienceModel({
+      ...testExperience,
+      creatorId: testUser._id
+    }).save();
+
+    const res = await request(app)
+      .delete(`/experiences/${insertedExperience._id}`)
+      .set("Cookie", sessionCookie);
 
     expect(res.status).toBe(200);
     
@@ -40,9 +74,11 @@ describe("DELETE /experiences/{experienceId}", () => {
     expect(retrievedExperience).toBeNull();
   });
 
-  it("should return a 500 code if given an invalid ID", async () => {
-    const res = await request(app).delete(`/experiences/1234`);
+  it("should return a 400 code if given an invalid ID", async () => {
+    const res = await request(app)
+      .delete(`/experiences/1234`)
+      .set("Cookie", sessionCookie);
 
-    expect(res.status).toBe(500);
+    expect(res.status).toBe(400);
   });
 });
--- src/utils/MockAuthStrategy.ts
+++ src/utils/MockAuthStrategy.ts
@@ -13,15 +13,15 @@ export class MockAuthStrategy extends Strategy {
         googleId: "12345",
         emailAddress: "test.user@fake.com",
         displayName: "Test User",
-        isModerator: isModerator ? true : false,
-        isAdmin: isAdmin ? true : false
+        isModerator: isModerator && isModerator === "true",
+        isAdmin: isAdmin && isAdmin === "true"
       };
 
       try {
         const mockUser = await new UserModel(user).save();
         this.success(mockUser);
       } catch(err) {
-        console.log("Failed to add mock user to database");
+        console.log(`Failed to add mock user to database: ${err}`);
         this.fail();
       }
   }
