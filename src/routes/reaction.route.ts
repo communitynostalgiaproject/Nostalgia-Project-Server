4d92e93a2ab608b4f8c8d02d5140a2a2c93e2029
David White <whited9339@gmail.com>
2023-12-11 03:03:16 -0700

    Created API routes for reactions

---
+++ src/routes/reaction.route.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { isAuthenticated, createAuthorizationMiddleware } from "../middleware/authChecks";
+import ReactionController from "../controllers/reactions.controller";
+import ReactionModel from "src/models/reaction.model";
+
+const router = Router();
+const isAuthorized = createAuthorizationMiddleware(ReactionModel, (user, document) => {
+  return user._id.equals(document.userId);
+});
+
+router.post("/", isAuthenticated, ReactionController.create);
+router.get("/:documentId", ReactionController.readById);
+router.get("/", ReactionController.read);
+router.patch("/:documentId", isAuthorized, ReactionController.update);
+router.delete("/:documentId", isAuthorized, ReactionController.delete);
+
+export default router;
