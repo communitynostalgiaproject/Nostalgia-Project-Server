f5016dcb5acd51861651c1db751afdbd4e2d7c63
David W <david@david-white.dev>
2023-12-24 14:05:19 -0800

    Added status and banCount fields to the Ban interface

--- types/ban.d.ts
+++ types/ban.d.ts
@@ -1,7 +1,11 @@
 import { ObjectId } from "mongoose";
 
+export type BanStatus = "active" | "restored"
+
 export interface Ban {
     userId: ObjectId,
     createdDate: Date,
     reason: string,
+    status: BanStatus,
+    banCount: number
 }
